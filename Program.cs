22c3a5ba589b9ec1eb959de4060343d8834fe8ff
eduuh <edwinkamaumuraya0@outlook.com>
2021-12-07 19:22:58 +0300

    Created a new project
    - learn csharp again. good for interview

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public void Writeline(string s)
+    {
+        System.Console.WriteLine(s);
+    }
+
+}
