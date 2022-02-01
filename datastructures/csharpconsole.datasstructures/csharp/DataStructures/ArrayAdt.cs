3b1d75b74e8e376c2e65b2cc6c93ca6d52a6debc
eduuh <edwinkamaumuraya0@outlook.com>
2022-02-01 20:41:11 +0300

    Added vimspector to the code base

--- datastructures/csharpconsole.datasstructures/csharp/DataStructures/ArrayAdt.cs
+++ datastructures/csharpconsole.datasstructures/csharp/DataStructures/ArrayAdt.cs
@@ -18,23 +18,140 @@ public class ArrayAdt
         Console.WriteLine("Hello there");
     }
 
-    public int SumArray() {
+    public int SumArray()
+    {
         int total = 0;
-       for (int i = 0; i < A.Length; i++)
-       {
-           total += A[i];
-       }
-       return total;
+        for (int i = 0; i < A.Length; i++)
+        {
+            total += A[i];
+        }
+        return total;
     }
+
+    public static int[,] twoDArrayExample()
+    {
+        int[,] arr = new int[4, 2];
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 2; j++)
+                arr[i, j] = count++;
+
+        }
+        return arr;
+    }
+
+    public static void print2Darray(int[,] arr, int row, int col)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Console.Write(" " + arr[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
 }
 
-internal static class ArrayExtensionMethods {
-    public static int SumArray(this int[] arr) {
+internal static class ArrayExtensionMethods
+{
+    public static int SumArray(this int[] arr)
+    {
         int total = 0;
-       for (int i = 0; i < arr.Length; i++)
-       {
-           total += arr[i];
-       }
-       return total;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            total += arr[i];
+        }
+        return total;
     }
+
+    public static int SequentialSearch(this int[] arr, int key)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int BinarySearch(this int[] arr, int key)
+    {
+        /// [o,1,2,3,4,5]
+        int low, mid, high;
+        low = 0;
+        high = arr.Length - 1;
+
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            if (arr[mid] == key)
+            {
+                return mid;
+            }
+            else if (key > arr[mid])
+            {
+                low = mid + 1;
+            }
+            else if (key < arr[mid])
+            {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void RotateRightFunction(this int[] arr, int times)
+    {
+
+        int temp, i, j;
+        i = 0;
+        j = arr.Length - 1;
+        for (; i < times; i++, j--)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void Display(this int[] arr)
+    {
+        foreach (var val in arr)
+        {
+            Console.Write(" " + val);
+        }
+    }
+
+    //intergers
+    public static void printInt1(int number)
+    {
+        char digit = (char)(number % 10 + '0');
+        number = number / 10;
+        if (number != 0)
+        {
+            printInt1(number / 10);
+        }
+        Console.Write("%c" + digit);
+    }
+
+    // base 16
+    public static void printInt2(int number, int baseValue)
+    {
+        string conversion = "0123456789ABCDEF";
+        char digit = (char)(number % baseValue);
+        number = number / baseValue;
+        if (number != 0)
+        {
+            printInt2(number, baseValue);
+        }
+        Console.Write(" " + conversion[digit]);
+    }
+
+
 }
--- datastructures/csharpconsole.datasstructures/csharp/Program.cs
+++ datastructures/csharpconsole.datasstructures/csharp/Program.cs
@@ -6,7 +6,35 @@
 
 //Sum Array
 int[] array= new int[5] {1, 2,3,4,5};
-WriteLine($"{array.SumArray()}");
+//WriteLine($"{array.SumArray()}");
+
+int[,] arr = ArrayAdt.twoDArrayExample();
+
+ArrayAdt.print2Darray(arr, 4, 2);
+
+int index = array.SequentialSearch(3);
+int bindex = array.BinarySearch(3);
+int bindex2 = array.BinarySearch(5);
+int bindex3 = array.BinarySearch(6);
+
+WriteLine($"index of 3 is: {index}");
+WriteLine($"index of 3 is: {bindex}");
+WriteLine($"index of 5 is: {bindex2}");
+WriteLine($"index of 5 is: {bindex3}");
+
+array.RotateRightFunction(2);
+array.Display();
+
+//print base 10 interger
+ArrayExtensionMethods.printInt1(25);
+System.Console.WriteLine();
+ArrayExtensionMethods.printInt2(25, 16);
+
+
+
+
+
+
 
 
 
